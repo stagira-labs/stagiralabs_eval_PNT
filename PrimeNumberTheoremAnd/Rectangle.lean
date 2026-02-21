9fa01806ba2842f9c431122f78bf90dd86bafc61
stagira-labs <stagiralabs@gmail.com>
2026-02-21 22:17:59 +0000

    [Agora] Modified: PrimeNumberTheoremAnd/Rectangle.lean

--- PrimeNumberTheoremAnd/Rectangle.lean
+++ PrimeNumberTheoremAnd/Rectangle.lean
@@ -66,17 +66,35 @@ lemma square_neg (p : ℂ) (c : ℝ) : Square p (-c) = Square p c := by
   simp [Square, Rectangle.symm, show (-c : ℂ) = -(↑c) by norm_cast, show (c : ℂ) = ↑c by rfl]
   ring_nf
 @[target]
-theorem Set.left_not_mem_uIoo {a b : ℝ} : a ∉ Set.uIoo a b := by sorry
+theorem Set.left_not_mem_uIoo {a b : ℝ} : a ∉ Set.uIoo a b := by
+  simp [Set.uIoo, uIoo]
+  intro; linarith
 @[target]
-theorem Set.right_not_mem_uIoo {a b : ℝ} : b ∉ Set.uIoo a b := by sorry
+theorem Set.right_not_mem_uIoo {a b : ℝ} : b ∉ Set.uIoo a b := by
+  simp [Set.uIoo, uIoo]
+  intro; linarith
 @[target]
-theorem Set.ne_left_of_mem_uIoo {a b c : ℝ} (hc : c ∈ Set.uIoo a b) : c ≠ a := by sorry
+theorem Set.ne_left_of_mem_uIoo {a b c : ℝ} (hc : c ∈ Set.uIoo a b) : c ≠ a := by
+  intro h
+  rw [h] at hc
+  simp [Set.uIoo, uIoo] at hc
+  linarith
 @[target]
-theorem Set.ne_right_of_mem_uIoo {a b c : ℝ} (hc : c ∈ Set.uIoo a b) : c ≠ b := by sorry
+theorem Set.ne_right_of_mem_uIoo {a b c : ℝ} (hc : c ∈ Set.uIoo a b) : c ≠ b := by
+  intro h
+  rw [h] at hc
+  simp [Set.uIoo, uIoo] at hc
+  linarith
 @[target]
-lemma left_mem_rect (z w : ℂ) : z ∈ Rectangle z w := by sorry
+lemma left_mem_rect (z w : ℂ) : z ∈ Rectangle z w := by
+  constructor
+  · simp [uIcc]
+  · simp [uIcc]
 @[target]
-lemma right_mem_rect (z w : ℂ) : w ∈ Rectangle z w := by sorry
+lemma right_mem_rect (z w : ℂ) : w ∈ Rectangle z w := by
+  constructor
+  · simp [uIcc]
+  · simp [uIcc]
 @[target]
 lemma rect_subset_iff {z w z' w' : ℂ} :
     Rectangle z' w' ⊆ Rectangle z w ↔ z' ∈ Rectangle z w ∧ w' ∈ Rectangle z w := by sorry
