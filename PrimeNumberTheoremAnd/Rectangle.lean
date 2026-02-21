d8f670d767bc1b6fa389b51094efe9ed71dfd491
stagira-labs <stagiralabs@gmail.com>
2026-02-21 22:15:59 +0000

    [Agora] Modified: PrimeNumberTheoremAnd/Rectangle.lean

--- PrimeNumberTheoremAnd/Rectangle.lean
+++ PrimeNumberTheoremAnd/Rectangle.lean
@@ -59,7 +59,8 @@ lemma rectangle_in_convex {U : Set ℂ} (U_convex : Convex ℝ U) {z w : ℂ} (h
     Rectangle z w ⊆ U := by sorry
 @[target]
 lemma mem_Rect {z w : ℂ} (zRe_lt_wRe : z.re ≤ w.re) (zIm_lt_wIm : z.im ≤ w.im) (p : ℂ) :
-    p ∈ Rectangle z w ↔ z.re ≤ p.re ∧ p.re ≤ w.re ∧ z.im ≤ p.im ∧ p.im ≤ w.im := by sorry
+    p ∈ Rectangle z w ↔ z.re ≤ p.re ∧ p.re ≤ w.re ∧ z.im ≤ p.im ∧ p.im ≤ w.im := by
+  simp only [Rectangle, mem_reProdIm, uIcc_of_le zRe_lt_wRe, uIcc_of_le zIm_lt_wIm, Set.mem_Icc, and_assoc]
 @[target]
 lemma square_neg (p : ℂ) (c : ℝ) : Square p (-c) = Square p c := by sorry
 @[target]
