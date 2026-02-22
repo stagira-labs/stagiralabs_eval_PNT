cf7c31739fe40142a199b5d73878403decf4bf0d
stagira-labs <stagiralabs@gmail.com>
2026-02-22 17:02:06 +0000

    [Agora] Modified: PrimeNumberTheoremAnd/Rectangle.lean

--- PrimeNumberTheoremAnd/Rectangle.lean
+++ PrimeNumberTheoremAnd/Rectangle.lean
@@ -83,9 +83,11 @@ theorem Set.ne_right_of_mem_uIoo {a b c : ℝ} (hc : c ∈ Set.uIoo a b) : c ≠
   simp [Set.uIoo, heq] at hc
   linarith
 @[target]
-lemma left_mem_rect (z w : ℂ) : z ∈ Rectangle z w := by sorry
+lemma left_mem_rect (z w : ℂ) : z ∈ Rectangle z w := by
+  constructor <;> simp
 @[target]
-lemma right_mem_rect (z w : ℂ) : w ∈ Rectangle z w := by sorry
+lemma right_mem_rect (z w : ℂ) : w ∈ Rectangle z w := by
+  constructor <;> simp
 @[target]
 lemma rect_subset_iff {z w z' w' : ℂ} :
     Rectangle z' w' ⊆ Rectangle z w ↔ z' ∈ Rectangle z w ∧ w' ∈ Rectangle z w := by sorry
